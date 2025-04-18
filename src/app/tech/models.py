86191d028a4636cf20354a28f49758296c6af230
ArtemBir <biryukov163ru@gmail.com>
2025-04-18 22:10:02 +0400

    Repair model

--- src/app/tech/models.py
+++ src/app/tech/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
 from src.app.customers.models import Customer
 
@@ -9,6 +10,15 @@ class CarType(models.Model):
 
     def __str__(self):
         return f'{self.make} {self.model}'
+
+
+class CarPart(models.Model):
+    name = models.CharField(max_length=100)
+    car_type = models.ForeignKey(CarType, on_delete=models.CASCADE, null=True, blank=True, related_name='parts')
+    in_stock = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Car(models.Model):
@@ -27,10 +37,36 @@ class Car(models.Model):
         state_display = self.get_state_display()
         return f'({self.car_type}) {self.year} {self.color} ({state_display}) {customer_info}'
 
+    def repair_start(self, car_part: CarPart):
+        if not car_part.in_stock:
+            raise ValidationError(f"Car part {car_part.name} is not in storage.")
 
-class CarPart(models.Model):
-    name = models.CharField(max_length=100)
-    car_type = models.ForeignKey(CarType, on_delete=models.CASCADE, null=True, blank=True, related_name='parts')
+        repair, created = Repair.objects.get_or_create(car=self)
+        repair.car_parts.add(car_part)
+
+        car_part.in_stock = False
+        car_part.save()
+
+        self.state = self.CarState.NO_SERVICE
+        self.save()
+
+    def repair_finish(self, car_part: CarPart):
+        try:
+            repair = Repair.objects.get(car=self)
+        except Repair.DoesNotExist:
+            raise ValueError(f"No repair found for car {self.id}")
+
+        repair.car_parts.remove(car_part)
+
+        if repair.car_parts.count() == 0:
+            repair.delete()
+            self.state = self.CarState.IN_SERVICE
+            self.save()
+
+
+class Repair(models.Model):
+    car = models.ForeignKey(Car, on_delete=models.CASCADE, related_name='repairs')
+    car_parts = models.ManyToManyField(CarPart, related_name='repairs')
 
     def __str__(self):
-        return self.name
+        return f'Repair for {self.car} started'
