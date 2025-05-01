ffcb607491fd6ce6aad08456a181f23b206cc6d0
ArtemBir <biryukov163ru@gmail.com>
2025-05-02 01:51:41 +0400

    repair fix

--- src/settings.py
+++ src/settings.py
@@ -45,8 +45,37 @@ INSTALLED_APPS = [
     'src.app.accounting',
 
 ]
+#i need cors settings
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+]
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_HEADERS = [
+    'authorization',
+    'content-type',
+]
+CORS_ALLOW_METHODS = [
+    'GET',
+    'POST',
+    'PUT',
+    'PATCH',
+    'DELETE',
+]
+CORS_ORIGIN_ALLOW_ALL = True
+
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000',
+]
+CORS_ORIGIN_REGEX_WHITELIST = [
+    r'http://localhost:3000',
+]
+
+INSTALLED_APPS += [
+    'corsheaders',
+]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
